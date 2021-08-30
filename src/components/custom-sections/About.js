373651d7054a0bbe6d2927337c40a0693917768c
TerrenceJChan <terrencechan.writer@gmail.com>
2021-08-30 17:22:16 -0400

    Small card descriptions

--- src/components/custom-sections/About.js
+++ src/components/custom-sections/About.js
@@ -9,10 +9,25 @@ const About = (props) => {
         <Section header={"About"}>
             <div className={s.FlexBox}>
                 <div className={s.FlexBox_flexItem___left}>
+                    <h3>Study Foci</h3>
+                </div>
+                <div className={s.FlexBox_flexItem___right}>
+                    <div className={s.Card___small}>Business (Major)</div> 
+                    <div className={s.Card___small}>Political Science (Major)</div> 
+                    <div className={s.Card___small}>Economics (Minor)</div> 
+                    <div className={s.Card___small}>International Trade (Minor)</div> 
+                </div>
+            </div>
+            <div className={s.FlexBox}>
+                <div className={s.FlexBox_flexItem___left}>
                     <h3>My Hobbies</h3>
                 </div>
                 <div className={s.FlexBox_flexItem___right}>
-                    <h3>My Hobbies</h3>
+                    <div className={s.Card___small}>Running</div> 
+                    <div className={s.Card___small}>Reading</div> 
+                    <div className={s.Card___small}>Bouldering</div> 
+                    <div className={s.Card___small}>Gaming</div> 
+                    <div className={s.Card___small}>Dragon Boating</div> 
                 </div>
             </div>
         </Section>
